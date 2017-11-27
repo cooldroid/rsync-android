79c3e9d07dfd0b9bb1d033751d709bc88229431b
CoolDroid <amit.bagaria@gmail.com>
2017-11-27 12:51:43 +0530

    Updated for custom build

--- build_android.sh
+++ build_android.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # build_android.sh - build rsync binaries for different mobile architectures using the Android NDK
 #
@@ -8,13 +8,13 @@
 # Whether or not to strip binaries (smaller filesize)
 STRIP=1
 
-API=12
-GCC=4.6
-NDK="/opt/android-ndk"
+API=21
+GCC=4.9
+NDK="/media/android/android-ndk-r10e"
 SYSPREFIX="${NDK}/platforms/android-${API}/arch-"
-ARCH=(arm x86 mips)
-PREFIX=(arm-linux-androideabi-  x86- mipsel-linux-android-)
-CCPREFIX=(arm-linux-androideabi- i686-linux-android- mipsel-linux-android-)
+ARCH=(arm)
+PREFIX=(arm-linux-androideabi-)
+CCPREFIX=(arm-linux-androideabi-)
 
 cd rsync/
 for I in $(seq 0 $((${#ARCH[@]} - 1))); do
@@ -28,6 +28,6 @@ done
 
 echo -en "\e[1;33m"
 for I in $(seq 0 $((${#ARCH[@]} - 1))); do
-	file "rsync-${ARCH[$I]}"
+	file "../rsync-${ARCH[$I]}"
 done
 echo -en "\e[0m"
