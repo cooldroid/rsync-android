1cae3f03427eac8f2f9312da7b896bd88e46e469
CoolDroid <amit.bagaria@gmail.com>
2018-01-03 11:50:46 +0530

    Updated for custom build

--- build_android.sh
+++ build_android.sh
@@ -8,19 +8,19 @@
 # Whether or not to strip binaries (smaller filesize)
 STRIP=1
 
-API=21
+API=24
 GCC=4.9
-NDK="/media/android/android-ndk-r10e"
+NDK="/media/android/android-ndk-r15c"
 SYSPREFIX="${NDK}/platforms/android-${API}/arch-"
-ARCH=(arm)
-PREFIX=(arm-linux-androideabi-)
-CCPREFIX=(arm-linux-androideabi-)
+ARCH=(arm arm64)
+PREFIX=(arm-linux-androideabi- aarch64-linux-android-)
+CCPREFIX=(arm-linux-androideabi- aarch64-linux-android-)
 
 cd rsync/
 for I in $(seq 0 $((${#ARCH[@]} - 1))); do
 	make clean
-	export CC="${NDK}/toolchains/${PREFIX[$I]}${GCC}/prebuilt/linux-x86_64/bin/${CCPREFIX[$I]}gcc --sysroot=${SYSPREFIX}${ARCH[$I]}"
-	./configure CFLAGS="-static" --host="${ARCH[$I]}"
+	export CC="${NDK}/toolchains/${PREFIX[$I]}${GCC}/prebuilt/linux-x86_64/bin/${CCPREFIX[$I]}gcc --sysroot=${SYSPREFIX}${ARCH[$I]} -D__ANDROID_API__=$API"
+	./configure CFLAGS="-static" --host="${PREFIX[$I]%?}"
 	make
 	(( $STRIP )) && ${NDK}/toolchains/${PREFIX[$I]}${GCC}/prebuilt/linux-x86_64/bin/${CCPREFIX[$I]}strip rsync
 	mv rsync "../rsync-${ARCH[$I]}"
